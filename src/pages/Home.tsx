db3b5abfb904149f8a7787d4d778b4259ea3a888
Mirko Mälicke <mirko@hydrocode.de>
2022-05-21 20:20:27 +0200

    added scroll behavior

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,30 @@
-import { Box } from "@mui/material";
+import { Box, Fab } from "@mui/material";
+import { useRef } from "react";
 import Header from "../components/Header";
 import OverviewMap from "../components/OverviewMap";
 import OverviewTable from "../components/OverviewTable";
 import ViewBox from "../components/ViewBox";
+import { ExpandMore } from "@mui/icons-material"
+import { table } from "console";
 
 const Home = () => {
+    // create some references for scrolling
+    const tableRef = useRef<HTMLDivElement>();
+    
+
     return (
         <Box sx={{margin: 0, padding: 0, width: '100vw', overflowX: 'hidden'}}>
             <Header />
             <OverviewMap />
 
-            <ViewBox centering>
-                <OverviewTable />
-            </ViewBox>
+            <Box sx={{flexGrow: 1, justifyContent: 'center', display: 'flex', marginTop: '-28px'}}>
+                <Fab color="primary" onClick={() => tableRef.current?.scrollIntoView({behavior: 'smooth'})}><ExpandMore /></Fab>
+            </Box>
+            <Box ref={tableRef}>
+                <ViewBox centering>
+                    <OverviewTable />
+                </ViewBox>
+            </Box>
         </Box>
     )
 }
