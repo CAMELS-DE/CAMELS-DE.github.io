47d1c04084d29069d10282deb9fc2e7b9ef1b1e2
Mirko Mälicke <mirko@hydrocode.de>
2022-12-24 12:05:11 +0100

    station click handler implemented

--- src/components/map-components/StationsLayer.tsx
+++ src/components/map-components/StationsLayer.tsx
@@ -1,7 +1,8 @@
 import cloneDeep from "lodash.clonedeep"
 import { CirclePaint, MapLayerMouseEvent } from "mapbox-gl"
+
 import { useEffect, useState } from "react"
-import { Layer, Source, useMap } from "react-map-gl"
+import { Layer, Source, Popup, useMap } from "react-map-gl"
 import { useData } from "../../context/data"
 import { StationSource } from "../../context/data.model"
 import { useLayers } from "../../context/layers"
@@ -25,6 +26,7 @@ const StationsLayer: React.FC = () => {
 
     // build the pain
     const paint = {
+        'circle-color-transition': {duration: 600},
         'circle-color': ['case', ['boolean', ['feature-state', 'hover'], false], 'purple', 'red'],
         'circle-opacity': ['case', ['boolean', ['feature-state', 'hover'], false], 1.0, 0.5],
         'circle-radius': ['case', ['boolean', ['feature-state', 'hover'], false], 8.5, 6],
@@ -36,49 +38,59 @@ const StationsLayer: React.FC = () => {
     // listen to hover events
     useEffect(() => {
         // if the map is already loaded, append the listeners
-        if (map.current) {
-            console.log('Armed callbacks')
-            // onEnter
-            map.current.on('mousemove', 'stations', (e: MapLayerMouseEvent) => {
-                // set all hover to false
-                src?.features.forEach(f => {
-                    map.current?.setFeatureState(
-                        {source: 'stations', id: f.id}, {hover: false}
-                    )
-                })
-                // activate only the first one
-                if (e.features && e.features.length > 0) {
-                    map.current?.setFeatureState(
-                        {source: 'stations', id: e.features[0].id}, {hover: true}
-                    )
-                }
-                // change the mouse pointer
-                map.current?.getCanvas().style.setProperty('cursor', 'pointer')
-            })
-            map.current.on('mouseleave', 'stations', (e: MapLayerMouseEvent) => {
-                // set all to leave
-                src?.features.forEach(f => {
-                    map.current?.setFeatureState(
-                        {source: 'stations', id: f.id}, {hover: false}
-                    )
-                })
-                // go back to default mouse style
-                map.current?.getCanvas().style.setProperty('cursor', 'default')
-            })
-
-            map.current.on('click', 'stations', (e: MapLayerMouseEvent) => {
-                if (e.features && e.features.length > 0) {
-                    console.log(e.features)
-                }
-            })
-
+        if (!map.current) {
+            return
         }
-    }, [map])
+
+        // onEnter
+        map.current.on('mousemove', 'stations', (e: MapLayerMouseEvent) => {
+            // set all hover to false
+            src?.features.forEach(f => {
+                map.current?.setFeatureState(
+                    {source: 'stations', id: f.id}, {hover: false}
+                )
+            })
+            // activate only the first one
+            if (e.features && e.features.length > 0) {
+                map.current?.setFeatureState(
+                    {source: 'stations', id: e.features[0].id}, {hover: true}
+                )
+            }
+            // change the mouse pointer
+            map.current?.getCanvas().style.setProperty('cursor', 'pointer')
+        })
+
+        // on mouse leave
+        map.current.on('mouseleave', 'stations', (e: MapLayerMouseEvent) => {
+            // set all to leave
+            src?.features.forEach(f => {
+                map.current?.setFeatureState(
+                    {source: 'stations', id: f.id}, {hover: false}
+                )
+            })
+            // go back to default mouse style
+            map.current?.getCanvas().style.setProperty('cursor', 'default')
+        })
+
+        // click Handler
+        map.current.on('click', 'stations', (e: MapLayerMouseEvent) => {
+            if (e.features && e.features.length > 0) {
+                const f = e.features[0] as GeoJSON.Feature<GeoJSON.Point>;
+                map.current?.flyTo({
+                    center: e.lngLat,
+                    zoom: 11,
+                    bearing: Math.random() * 360,
+                    pitch: Math.random() * 30 + 30
+                })
+            }
+        })
+    }, [map, src])
     
     return <>
         <Source id="stations" type="geojson" data={src}>
             <Layer id="stations" type="circle" source="stations" paint={paint} layout={{'visibility': active.includes('stations') ? 'visible' : 'none'}}/>
         </Source>
+        
     </>
 }
 
